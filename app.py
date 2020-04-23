298fc9155879e2147581ef03fbfd4f8d75ee5025
Brian <bmoses00@stuy.edu>
2020-04-23 16:39:26 -0400

    Changed to using api call

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +17,19 @@ def adventure1():
 @app.route('/line_graphs')
 def line_graphs():
     return render_template("line_graphs.html")
+
+@app.route("/<string:data>")
+def data(data):
+    payload = []
+    with open(f"static/csv/{data}.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        # skip the first row, which is the header row
+        next(reader)
+        for entry in reader:
+            if entry[1] == ".":
+                continue
+            payload.append({'date': entry[0], 'yield': float(entry[1])})
+    return jsonify(payload)
 
 if __name__ == "__main__":
         app.debug = True
--- static/js/line_graphs.js
+++ static/js/line_graphs.js
@@ -13,44 +13,41 @@ svg = d3.select("#graph")
 const scaleX = d3.scaleTime().range([0, width - margin])
 const scaleY = d3.scaleLinear().range([height - margin, 0]);
 
-const parseTime = d3.timeParse("%Y-%m-%d");
+const parseDate = d3.timeParse("%Y-%m-%d");
 
 function draw_graph(dataset) {
-  d3.csv("static/csv/" + dataset).then(function(data) {
 
-    data.forEach(function(d, index) {
-      d.Date = parseTime(d.Date);
-      // some quarters have no data, for these, we set the yield to that of the previous quarter
-      if (d.Yield == ".") {
-        d.Yield = +data[index - 1].Yield;
-      }
-      else {
-      d.Yield = +d.Yield;
+  fetch("/" + dataset)
+    .then(response => response.json())
+    .then(data => {
+
+      for (const entry of data) {
+           entry.date = parseDate(entry.date);
       }
 
-    });
+      scaleX.domain(d3.extent(data, function(d) { return d.date; }));
+      scaleY.domain([0, d3.max(data, function(d) { return d.yield; })]);
 
-    scaleX.domain(d3.extent(data, function(d) { return d.Date; }));
-    scaleY.domain([0, d3.max(data, function(d) { return d.Yield; })]);
+      let line = d3.line()
+        .x(d => scaleX(d.date))
+        .y(d => scaleY(d.yield));
 
-    let line = d3.line()
-      .x(d => scaleX(d.Date))
-      .y(d => scaleY(d.Yield));
+      // draws line graph
+      svg.append("path")
+        .data([data])
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 
-    svg.append("path")
-       .data([data])
-       .attr("fill", "none")
-       .attr("stroke", "steelblue")
-       .attr("stroke-width", 1.5)
-       .attr("d", line);
+      // x and y axes
+      svg.append("g")
+        .attr("transform", "translate(0," + (height - margin) + ")")
+        .call(d3.axisBottom(scaleX));
 
-     svg.append("g")
-      .attr("transform", "translate(0," + (height - margin) + ")")
-      .call(d3.axisBottom(scaleX));
-
-     svg.append("g")
-       .call(d3.axisLeft(scaleY));
+      svg.append("g")
+        .call(d3.axisLeft(scaleY));
   });
 }
 
-draw_graph("10_year_treasury_yields.csv")
+draw_graph("10_year_treasury_yields")
