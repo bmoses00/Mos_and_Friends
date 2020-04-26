249195b1b4307e50ef8c0e28f1b58bc3f30b01d7
athompson00 <athompson00@stuy.edu>
2020-04-26 13:10:09 -0400

    addgraphinput works, but we need to actually add the graphs

--- static/js/create-study.js
+++ static/js/create-study.js
@@ -2,9 +2,12 @@ var index = 1;
 
 var addtextinput = function() {
   //remove previous item in list
-  var oldItem = document.getElementById("masterlist").children[index];
-  //console.log(document.getElementById("masterlist"));
-  oldItem.remove();
+  d3.selectAll("li")
+    .each(function(d, i) {
+      if (i == index) {
+        this.remove();
+      }
+    });
   //create a new input group for text
   var text = document.createElement("div");
   text.className += "input-group";
@@ -43,13 +46,16 @@ var addtextinput = function() {
 
 var removeCurrent = function() {
   //remove old
-  var oldItem = document.getElementById("masterlist").children[index];
-  //console.log(document.getElementById("masterlist"));
-  oldItem.remove();
+  d3.selectAll("li")
+    .each(function(d, i) {
+      if (i == index) {
+        this.remove();
+      }
+    });
   //create new list item
   var listItem = document.createElement("li");
   listItem.className += "list-group-item";
-  listItem.innerHTML += "<br>";
+  //listItem.innerHTML += "<br>";
   //Create add text button
   var textbutton = document.createElement("button");
   textbutton.innerHTML = " Add Text ";
@@ -72,13 +78,18 @@ var removeCurrent = function() {
   list.appendChild(listItem);
 }
 
-var finalizeText = function(){
-  //remove old
+var finalizeText = function() {
+  //get old
   var oldItem = document.getElementById("masterlist").children[index];
   //get text in before removing
   var textin = oldItem.children[0].children[1].value;
-  console.log(textin);
-  oldItem.remove();
+  //remove old
+  d3.selectAll("li")
+    .each(function(d, i) {
+      if (i == index) {
+        this.remove();
+      }
+    });
   //create new list item
   var listItem = document.createElement("li");
   listItem.className += "list-group-item";
@@ -113,4 +124,207 @@ var finalizeText = function(){
   //add new list item to list
   var list = document.getElementById("masterlist");
   list.appendChild(listItem);
+}
+
+
+var addgraphinput = function() {
+  //remove previous item in list
+  d3.selectAll("li")
+    .each(function(d, i) {
+      if (i == index) {
+        this.remove()
+      }
+    });
+
+  //create new list item
+  var listitem = d3.select("#masterlist")
+    .append("li")
+    .attr("class", "list-group-item")
+    .html("<br>");
+
+  //create structure for graph input
+  var structure = listitem
+    .append("div")
+    .attr("class", "row");
+
+  //create col/form for subject
+  var subjectCol = structure
+    .append("div")
+    .attr("class", "col")
+    .html("Choose Subject:");
+
+  subjectCol.node().innerHTML += "<br><br>";
+
+  //create select for subject dropdown menu
+  var subjectDropdown = subjectCol
+    .append("div")
+    .attr("class", "dropdown");
+
+  var subjectButton = subjectDropdown
+    .append("button")
+    .attr("class", "btn btn-secondary dropdown-toggle")
+    .attr("type", "button")
+    .attr("id", "subjectbutton")
+    .attr("data-toggle", "dropdown")
+    .html("Subject");
+
+  var subjectOptions = subjectDropdown
+    .append("div")
+    .attr("class", "dropdown-menu");
+
+  //add Options to dropdown
+  //will be down with a for loop through list of subjects, but for now j adding samples
+  subjectOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 1");
+  subjectOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 2");
+
+  //create col/form for dates
+  var dateCol = structure
+    .append("div")
+    .attr("class", "col");
+
+
+  //create row for start date
+  var startRow = dateCol
+    .append("div")
+    .attr("class", "row")
+    .html("Start Date: ");
+
+  startRow.node().innerHTML += "&ensp;&ensp;";
+
+  //create start date dropdowns
+  var startYearDropdown = startRow
+    .append("div")
+    .attr("class", "dropdown");
+
+  var startYearButton = startYearDropdown
+    .append("button")
+    .attr("class", "btn btn-secondary dropdown-toggle")
+    .attr("type", "button")
+    .attr("id", "startyearbutton")
+    .attr("data-toggle", "dropdown")
+    .html("Year");
+
+  var startyearOptions = startYearDropdown
+    .append("div")
+    .attr("class", "dropdown-menu");
+
+  //we will add specific years later
+  startyearOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 1");
+  startyearOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 2");
+
+  startRow.node().innerHTML += "&ensp;&ensp;";
+  //start month dropdown
+  var startMonthDropdown = startRow
+    .append("div")
+    .attr("class", "dropdown");
+
+  var startMonthbutton = startMonthDropdown
+    .append("button")
+    .attr("class", "btn btn-secondary dropdown-toggle")
+    .attr("type", "button")
+    .attr("id", "startmonthbutton")
+    .attr("data-toggle", "dropdown")
+    .html("Month");
+
+  var startmonthOptions = startMonthDropdown
+    .append("div")
+    .attr("class", "dropdown-menu");
+  var i = 0;
+  for (i = 0; i < 12; i++) {
+    startmonthOptions.append("a").attr("class", "dropdown-item").html((i + 1) + "");
+  }
+
+  dateCol.node().innerHTML += "<br><br>";
+  //create row for end date
+  var endRow = dateCol
+    .append("div")
+    .attr("class", "row")
+    .html("End Date: ");
+
+  endRow.node().innerHTML += "&ensp;&ensp;&ensp;";
+  //create start date dropdowns
+  var endYearDropdown = endRow
+    .append("div")
+    .attr("class", "dropdown");
+
+  var endYearButton = endYearDropdown
+    .append("button")
+    .attr("class", "btn btn-secondary dropdown-toggle")
+    .attr("type", "button")
+    .attr("id", "endyearbutton")
+    .attr("data-toggle", "dropdown")
+    .html("Year");
+
+  var endyearOptions = endYearDropdown
+    .append("div")
+    .attr("class", "dropdown-menu");
+
+  //we will add specific years later
+  endyearOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 1");
+  endyearOptions
+    .append("a")
+    .attr("class", "dropdown-item")
+    .html("option 2");
+
+  endRow.node().innerHTML += "&ensp;&ensp;";
+  //start month dropdown
+  var endMonthDropdown = endRow
+    .append("div")
+    .attr("class", "dropdown");
+
+  var endMonthbutton = endMonthDropdown
+    .append("button")
+    .attr("class", "btn btn-secondary dropdown-toggle")
+    .attr("type", "button")
+    .attr("id", "endmonthbutton")
+    .attr("data-toggle", "dropdown")
+    .html("Month");
+
+  var endmonthOptions = endMonthDropdown
+    .append("div")
+    .attr("class", "dropdown-menu");
+  var i = 0;
+  for (i = 0; i < 12; i++) {
+    endmonthOptions.append("a").attr("class", "dropdown-item").html((i + 1) + "");
+  }
+
+  //add render/back buttons
+  var finalCol = structure
+    .append("div")
+    .attr("class", "col");
+
+  var backbutton = finalCol
+    .append("button")
+    .attr("class", "btn btn-secondary")
+    .attr("id", "backbutton")
+    .attr("onclick", "removeCurrent()")
+    .html("Back");
+
+  finalCol.node().innerHTML += "<br><br>";
+
+  var renderbutton = finalCol
+    .append("button")
+    .attr("class", "btn btn-secondary")
+    .attr("id", "renderbutton")
+    .attr("onclick", "renderGraph()")
+    .html("Render Graph");
+
+
+
+  console.log(structure.node());
 }
