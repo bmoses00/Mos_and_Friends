8cdbf2282017e0df2b3107884cce6bc3120051d1
William Cao <wcao00@stuy.edu>
2020-04-27 17:46:10 -0400

    don't load econ data into mongo

--- data/database_creator.py
+++ data/database_creator.py
@@ -4,46 +4,6 @@ import csv
 # Recreate the database
 database = mongo_client[DATABASE_NAME]
 
-def load_econ_data_sets():
-    """
-    Each econ data collection will be formatted as:
-    {
-        name: string,
-        start_date: "YYYY-MM-DD",
-        end_date: "YYYY-MM-DD",
-        // first element will always be date
-        column_names: [string, string]
-        data: [
-                {
-                    date: "YYYY-MM-DD",
-                    value: number
-                },
-                {
-                    date: "YYYY-MM-DD",
-                    value: number
-                },
-                ...
-            ]
-        }
-    }
-    """
-    for info in econ_data_info:
-        with open(f"static/csv/{info['file_name']}") as csv_file:
-            csv_data = list(csv.reader(csv_file))
-        # csv first row is header with name of each row
-        column_names = csv_data.pop(0)
-        data = []
-        for element in csv_data:
-            data.append({"date": element[0], "value": element[1]})
-        name = info["name"]
-        database[DATA_COLLECTION].insert({
-            "name": name,
-            "start_date": data[0]["date"],
-            "end_date": data[len(data) - 1]["date"],
-            "column_names": column_names,
-            "data": data
-        })
-
 
 def create_users_system():
     """
@@ -110,6 +70,5 @@ def recreate_database():
         mongo_client.drop_database(DATABASE_NAME)
 
     # recreate data
-    load_econ_data_sets()
     create_users_system()
     create_case_studies_collection()
