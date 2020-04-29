c671fc8de21187bf4c6b9b54d83c1a7ebe8382be
athompson00 <athompson00@stuy.edu>
2020-04-28 23:10:41 -0400

    added proper delete method and switched flash messages to base.html

--- app/__init__.py
+++ app/__init__.py
@@ -131,14 +131,14 @@ def create_study():
         return render_template("create-study.html", data_sets=econ_data, data_sets_json = json.dumps(econ_data))
 
 
-@app.route("/delete-study/<string:id>", methods=["DELETE", "GET"])
+@app.route("/delete-study/<string:id>", methods=["DELETE"])
 def delete_study(id: str):
     case_study = database_query.get_case_study(id)
     if case_study is None:
         return "Case study does not exist. Cannot delete", 400
     elif "username" in session and session["username"] == case_study["username"]:
         database_query.delete_case_study(id)
-        return redirect(url_for("view_studies"))
+        return "", 200
     else:
         return "No permission", 400
 
