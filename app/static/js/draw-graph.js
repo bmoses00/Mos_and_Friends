b6f95155f192bb1b2e0097395f2654375a3534e1
Brian <bmoses00@stuy.edu>
2020-05-03 13:10:45 -0400

    Added multiple graphs fixes

--- app/static/js/draw-graph.js
+++ app/static/js/draw-graph.js
@@ -9,149 +9,166 @@ function draw_graph(element, dataset, dataset_2, year_start, year_end, first_ren
 
   if (r == null) r = ""; // todo: use default parameter
 
-  d3.csv("../static/csv/" + dataset + ".csv").then(function(raw_data) {
-   data = []
+  if (dataset != null) {
+    d3.csv("../static/csv/" + dataset + ".csv").then(function(raw_data) {
+     data = []
 
-   const date = raw_data.columns[0];
-   const value = raw_data.columns[1];
+     const date = raw_data.columns[0];
+     const value = raw_data.columns[1];
 
-   raw_data.forEach(function(d, index) {
-     const current_year = d[date].substring(0, 4)
-     if (d[value] != "." && current_year >= year_start && current_year <= year_end ) {
+     raw_data.forEach(function(d, index) {
+       const current_year = d[date].substring(0, 4)
+       if (d[value] != "." && current_year >= year_start && current_year <= year_end ) {
 
-       d[value] = +d[value];
-       d[date] = parseTime(d[date]);
-       data.push(d);
+         d[value] = +d[value];
+         d[date] = parseTime(d[date]);
+         data.push(d);
+       }
+     });
+     if (!first_render) {
+       d3.select("#path" + r).remove();
+       d3.select("#x-axis" + r).remove();
+       d3.select("#y-axis" + r).remove();
+       d3.select("#x-label" + r).remove();
+       d3.select("#y-label" + r).remove();
      }
+
+     scaleX.domain(d3.extent(data, function(d) { return d[date]; }));
+
+     let domain_lower;
+     let translate;
+     if (d3.min(data, function(d) { return d[value]; }) < 0)
+       domain_lower = d3.min(data, function(d) { return d[value]; })
+     else
+       domain_lower = 0;
+
+     scaleY.domain([domain_lower, d3.max(data, function(d) { return d[value]; })]);
+
+     let line = d3.line()
+       .x(d => scaleX(d[date]))
+       .y(d => scaleY(d[value]));
+
+
+     // draws line graph
+     let line_path = element.append("path")
+       .data([data])
+       .attr("id", "path" + r)
+       .attr("fill", "none")
+       .attr("stroke", "steelblue")
+       .attr("stroke-width", 1.5)
+       .attr("d", line);
+
+     let total_length = line_path.node().getTotalLength();
+
+     line_path
+       .attr("stroke-dasharray", total_length)
+       .attr("stroke-dashoffset", total_length)
+       .transition()
+       .duration(2000)
+       .attr("stroke-dashoffset", 0);
+
+     // x and y axes
+     element.append("g")
+       .attr("id", "y-axis" + r)
+       .call(d3.axisLeft(scaleY));
+     element.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("id", "y-label" + r)
+        .attr("y", 0 - margin)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("fill", "steelblue")
+        .style("text-anchor", "middle")
+        .text(value);
    });
-   if (!first_render) {
-     d3.select("#path" + r).remove();
-     d3.select("#x-axis" + r).remove();
-     d3.select("#y-axis" + r).remove();
-     d3.select("#x-label" + r).remove();
-     d3.select("#y-label" + r).remove();
-   }
+ }
 
-   scaleX.domain(d3.extent(data, function(d) { return d[date]; }));
+ if (dataset_2 != null) {
+   d3.csv("../static/csv/" + dataset_2 + ".csv").then(function(raw_data) {
+     data = []
 
-   let domain_lower;
-   let translate;
-   if (d3.min(data, function(d) { return d[value]; }) < 0)
-     domain_lower = d3.min(data, function(d) { return d[value]; })
-   else
-     domain_lower = 0;
+     const date = raw_data.columns[0];
+     const value = raw_data.columns[1];
 
-   scaleY.domain([domain_lower, d3.max(data, function(d) { return d[value]; })]);
+     raw_data.forEach(function(d, index) {
+       const current_year = d[date].substring(0, 4)
+       if (d[value] != "." && current_year >= year_start && current_year <= year_end ) {
 
-   let line = d3.line()
-     .x(d => scaleX(d[date]))
-     .y(d => scaleY(d[value]));
-
-
-   // draws line graph
-   let line_path = element.append("path")
-     .data([data])
-     .attr("id", "path" + r)
-     .attr("fill", "none")
-     .attr("stroke", "steelblue")
-     .attr("stroke-width", 1.5)
-     .attr("d", line);
-
-   let totalLength = line_path.node().getTotalLength();
-
-   line_path
-     .attr("stroke-dasharray", totalLength)
-     .attr("stroke-dashoffset", totalLength)
-     .transition()
-     .duration(2000)
-     .attr("stroke-dashoffset", 0);
-
-   // x and y axes
-   element.append("g")
-     .attr("id", "x-axis" + r)
-     .attr('transform', 'translate(0,' + (height - margin) + ')')
-     .call(d3.axisBottom(scaleX));
-   element.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("id", "y-label" + r)
-      .attr("y", 0 - margin)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("fill", "steelblue")
-      .style("text-anchor", "middle")
-      .text(value);
-   element.append("g")
-     .attr("id", "y-axis" + r)
-     .call(d3.axisLeft(scaleY));
-   element.append("text")
-      .attr("transform", "translate(" + (width/2 - margin)+ "," + height + ")")
-      .attr("id", "x-label" + r)
-      .style("text-anchor", "middle")
-      .text("Year");
- });
-
- d3.csv("../static/csv/" + dataset_2 + ".csv").then(function(raw_data) {
-   data = []
-
-   const date = raw_data.columns[0];
-   const value = raw_data.columns[1];
-
-   raw_data.forEach(function(d, index) {
-     const current_year = d[date].substring(0, 4)
-     if (d[value] != "." && current_year >= year_start && current_year <= year_end ) {
-
-       d[value] = +d[value];
-       d[date] = parseTime(d[date]);
-       data.push(d);
+         d[value] = +d[value];
+         d[date] = parseTime(d[date]);
+         data.push(d);
+       }
+     });
+     if (!first_render) {
+       d3.select("#path2" + r).remove();
+       d3.select("#x-axis2" + r).remove();
+       d3.select("#y-axis2" + r).remove();
+       d3.select("#y-label2" + r).remove();
      }
+
+     scaleX.domain(d3.extent(data, function(d) { return d[date]; }));
+
+     let domain_lower;
+     let translate;
+     if (d3.min(data, function(d) { return d[value]; }) < 0)
+       domain_lower = d3.min(data, function(d) { return d[value]; })
+     else
+       domain_lower = 0;
+
+     scaleY.domain([domain_lower, d3.max(data, function(d) { return d[value]; })]);
+
+     let line = d3.line()
+       .x(d => scaleX(d[date]))
+       .y(d => scaleY(d[value]));
+
+     // draws line graph
+     let line_path_2 = element.append("path")
+       .data([data])
+       .attr("id", "path2" + r)
+       .attr("fill", "none")
+       .attr("stroke", "red")
+       .attr("stroke-width", 1.5)
+       .attr("d", line);
+
+     let total_length_2 = line_path_2.node().getTotalLength();
+
+     line_path_2
+       .attr("stroke-dasharray", total_length_2)
+       .attr("stroke-dashoffset", total_length_2)
+       .transition()
+       .duration(2000)
+       .attr("stroke-dashoffset", 0);
+
+     // x and y axes
+     element.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("id", "y-label2" + r)
+        .attr("y", width - (2 * margin) + 20)
+        .attr("x", -(height / 2))
+        .attr("dy", "1em")
+        .attr("fill", "red")
+        .style("text-anchor", "middle")
+        .text(value);
+
+     element.append("g")
+       .attr("id", "y-axis2" + r)
+       .attr('transform', 'translate(' + (width - (2 * margin) - 10)+ ',0)')
+       .call(d3.axisRight(scaleY));
    });
-   if (!first_render) {
-     d3.select("#path2" + r).remove();
-     d3.select("#x-axis2" + r).remove();
-     d3.select("#y-axis2" + r).remove();
-     d3.select("#y-label2" + r).remove();
-   }
+ }
 
-   scaleX.domain(d3.extent(data, function(d) { return d[date]; }));
+ d3.select("x-axis" + r).remove();
+ d3.select("x-label" + r).remove();
 
-   let domain_lower;
-   let translate;
-   if (d3.min(data, function(d) { return d[value]; }) < 0)
-     domain_lower = d3.min(data, function(d) { return d[value]; })
-   else
-     domain_lower = 0;
-
-   scaleY.domain([domain_lower, d3.max(data, function(d) { return d[value]; })]);
-
-   let line = d3.line()
-     .x(d => scaleX(d[date]))
-     .y(d => scaleY(d[value]));
-
-   // draws line graph
-   element.append("path")
-     .data([data])
-     .attr("id", "path2" + r)
-     .attr("fill", "none")
-     .attr("stroke", "red")
-     .attr("stroke-width", 1.5)
-     .attr("d", line);
-
-   // x and y axes
-   element.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("id", "y-label2" + r)
-      .attr("y", width - (2 * margin) + 20)
-      .attr("x", -(height / 2))
-      .attr("dy", "1em")
-      .attr("fill", "red")
-      .style("text-anchor", "middle")
-      .text(value);
-
-   element.append("g")
-     .attr("id", "y-axis2" + r)
-     .attr('transform', 'translate(' + (width - (2 * margin) - 10)+ ',0)')
-     .call(d3.axisRight(scaleY));
- });
+ element.append("g")
+   .attr("id", "x-axis" + r)
+   .attr('transform', 'translate(0,' + (height - margin) + ')')
+   .call(d3.axisBottom(scaleX));
+ element.append("text")
+   .attr("transform", "translate(" + (width/2 - margin)+ "," + height + ")")
+   .attr("id", "x-label" + r)
+   .style("text-anchor", "middle")
+   .text("Year");
 }
 
 function get_true_start_year(dataset, dataset_2) {
